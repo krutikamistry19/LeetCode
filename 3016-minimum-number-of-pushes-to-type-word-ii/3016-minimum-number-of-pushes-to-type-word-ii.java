042614a0a1143fba5aa51b4d911837ccd102e328
krutikamistry19 <krutikavishwakarma29@gmail.com>
2026-07-31 22:11:36 +0530

    Time: 10 ms (66.76%), Space: 48 MB (73.7%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int minimumPushes(String word) {
+        int[] freq = new int[26];
+
+        for (char ch : word.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        Integer[] arr = new Integer[26];
+        for (int i = 0; i < 26; i++) {
+            arr[i] = freq[i];
+        }
+
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        int ans = 0;
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] == 0) break;
+
+            int pushes = (i / 8) + 1;
+            ans += arr[i] * pushes;
+        }
+
+        return ans;
+    }
+}
